9c91ac423712219479e340d0ed6facf16b74bcf5
Itaf Omar Joudeh <itaf.on.line@hotmail.com>
2016-11-14 22:06:28 -0500

    Update singly_linked_list.h

--- singly_linked_list.h
+++ singly_linked_list.h
@@ -2,28 +2,29 @@
 
 /* A node in a singly-linked list that stores values of type int. */
 
-struct intnode {
+struct intnode 
+{
     int    value;
     struct intnode *next;
 };
 
 typedef struct intnode IntNode;
 
+// Functions
 IntNode *intnode_construct(int value, IntNode *next);
-
 void print_linked_list(IntNode *p);
 _Bool contains(IntNode *head, int value);
 _Bool contains_shorter(IntNode *head, int value);
+
 IntNode *insert_front(IntNode *head, int value);
 IntNode *append_rear(IntNode *head, int value);
 IntNode *remove_first(IntNode *head);
 IntNode *remove_last(IntNode *head);
+IntNode *remove_last_one_pointer(IntNode *head);
 
 int count(IntNode *head, int target);
 int index(IntNode *head, int target);
 int fetch(IntNode *head, int index);
-IntNode *remove_last_one_pointer(IntNode *head);
-
 
 IntNode *insert(IntNode *head, int index, int x);
 IntNode *delete(IntNode *head, int index);
